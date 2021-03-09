e27d85931845be932d7f6767b15f3f7308e76097
ganeshmastud <mastudganesh6@gmail.com>
2021-03-09 14:02:42 +0530

    updated get_prd_amazon function

--- prd_info.py
+++ prd_info.py
@@ -10,18 +10,24 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
 
 #selenium.common.exceptions.StaleElementReferenceException:
-def get_prd_amazon(search):
+def get_prd_amazon(search,no_of_products):
     driver = webdriver.Chrome('chromedriver.exe')
     driver.maximize_window()
     search = "+".join(search.split(" "))
-    if "+" in search:
-        search_url="https://www.amazon.in/s?k={k}&crid=2PT07K1CUD2LT&sprefix=earphone%2Caps%2C294&ref=nb_sb_ss_ts-doa-p_1_8"
-        driver.get(search_url.format(k=search))
-    else:
-        search_url = "https://www.amazon.in/s?k={k}&ref=nb_sb_noss_2"
-        driver.get(search_url.format(k=search))
+    # if "+" in search:
+    #     search_url="https://www.amazon.in/s?k={k}&crid=2PT07K1CUD2LT&sprefix=earphone%2Caps%2C294&ref=nb_sb_ss_ts-doa-p_1_8"
+    #     driver.get(search_url.format(k=search))
+    # else:
+    #     search_url = "https://www.amazon.in/s?k={k}&ref=nb_sb_noss_2"
+    #     driver.get(search_url.format(k=search))
     driver.implicitly_wait(2)
-    prds_likns = driver.find_elements_by_class_name('sg-col-inner') #InvalidSelectorException:
+    driver.get('https://www.amazon.in')
+    time.sleep(2)
+    driver.find_element_by_id('twotabsearchtextbox').send_keys(search)
+    driver.find_element_by_id('nav-search-submit-button').click()
+    time.sleep(5)
+    prds_likns = driver.find_elements_by_xpath("//a[@class='a-size-base a-link-normal s-no-hover a-text-normal']") #InvalidSelectorException:
+
     # prds_likns=prds_likns.find_element_by_class_name('sg-row')
     # prds_likns = prds_likns.find_element_by_class_name('sg-col-inner')
     # prds_likns = prds_likns.find_element_by_class_name('a-section a-spacing-none')
@@ -36,7 +42,7 @@ def get_prd_amazon(search):
     x = 0
     parent = driver.current_window_handle
     for lnk in prds_likns:
-        prd_details={"url": [],"name":[], "source": [], "price": [], "model no": []}
+        prd_details={"name":[], "source": [], "price": [], "model no": [],"category":[]}
         lnk.click()
         x += 1
         handles = driver.window_handles
@@ -46,23 +52,47 @@ def get_prd_amazon(search):
             print(driver.title)
             if x == 2:
                 time.sleep(3)
-                prd_details["url"].append(lnk.text)
+                # prd_details["url"].append(lnk.text)
                 prd_details["source"].append("Amazon")
-                print("name:", driver.find_element_by_class_name('a-size-large product-title-word-break').text)
-                prd_details["name"].append(driver.find_element_by_class_name('a-size-large product-title-word-break').text)
-                model_num = driver.find_elements_by_class_name('a-size-base prodDetAttrValue')
+                print("name:", driver.find_element_by_id('productTitle').text)
+                prd_details["name"].append(driver.find_element_by_id('productTitle').text)
+                model_num = driver.find_element_by_id('productDetails_techSpec_section_1')
+                row=model_num.find_elements_by_tag_name('tr')[4]
+                col=row.find_element_by_tag_name('td')
+                print(col.text)
 
-                print("model_no", model_num[4].text)
-                prd_details["model no"].append(model_num[4].text)
-                print("prize:", driver.find_element_by_class_name('a-size-medium a-color-price priceBlockDealPriceString').text)
-                prd_details["price"].append(driver.find_element_by_class_name('a-size-medium a-color-price priceBlockDealPriceString').text)
+                prd_details["model no"].append(col.text)
+                table = driver.find_element_by_id('productDetails_detailBullets_sections1')
+                row = table.find_elements_by_tag_name('tr')[6]
+                category=row.find_element_by_tag_name('td')
+                try:
+                    category = row.find_element_by_tag_name('td')
+                except Exception as e:
+                    category="Not available"
+                print("category",category.text)
+                prd_details["category"].append(category.text)
+                price=""
+                if len(price)==0:
+                    try:
+                        price=driver.find_element_by_id("priceblock_dealprice").text
+                    except Exception as e:
+                        price=""
+                if len(price)==0:
+                    try:
+                        price= driver.find_element_by_id('priceblock_saleprice').text
+                    except Exception as e:
+                        price = ""
+                if len(price)==0:
+                    price = driver.find_element_by_id("priceblock_ourprice").text
+                print("prize:", price)
+                prd_details["price"].append(price)
                 driver.close()
                 driver.switch_to.window(parent)
                 time.sleep(5)
                 x = 0
                 break
             x += 1
-        if cnt == 10:
+        if cnt == no_of_products-1:
             break
         cnt += 1
         time.sleep(5)
@@ -71,31 +101,26 @@ def get_prd_amazon(search):
     time.sleep(5)  # sleep_between_interactions
     driver.close()
     print(prd_details)
-    return
+    return prd_details
 
-def get_prd_flpkrt(search,feature):
+def get_prd_flpkrt(search, no_of_products):
     driver = webdriver.Chrome('chromedriver.exe')
     driver.maximize_window()
-    prd_dtl={"name:"}
-
+    driver.get('https://www.flipkart.com')
+    button = driver.find_element_by_xpath("/html/body/div[2]/div/div/button")
+    button.click()
     search = "+".join(search.split(" "))
-    print("search:",search)
-    # url="https://www.flipkart.com/search?q=mobiles&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
-    # driver.get(url)
-    # driver.find_element_by_class_name('_3704LK').clear()
-    #
-    # driver.find_element_by_class_name('_3704LK').send_keys(search)
-    # driver.find_element_by_class_name('L0Z3Pu').click()
-
-    search_url="https://www.flipkart.com/search?q={q}&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&as-pos=1&as-type=RECENT&suggestionId=poco+m3%7CMobiles&requestId=95ef28c9-955b-46ef-b7c3-8330d79e2900&as-backfill=on"
-
-    driver.get(search_url.format(q=search))
+    print("search:", search)
+    driver.find_element_by_class_name("_3704LK").send_keys(search)
+    time.sleep(2)
+    driver.find_element_by_class_name('L0Z3Pu').click()
+    time.sleep(8)
     prds_likns=driver.find_elements_by_class_name('_4rR01T')
     time.sleep(1)
     cnt=0
     x=0
     parent = driver.current_window_handle
-    prd_details = {"name": [], "source": [], "price": [], "model no": []}
+    prd_details = {"name": [], "source": [], "price": [], "model no": [],"category":[]}
     for lnk in prds_likns:
 
         lnk.click()
@@ -118,13 +143,16 @@ def get_prd_flpkrt(search,feature):
                 prd_details["model no"].append(model_num[1].text)
                 print("price:",driver.find_element_by_class_name('_30jeq3').text)
                 prd_details["price"].append(driver.find_element_by_class_name('_30jeq3').text)
+                category=model_num[4].text
+                prd_details["category"].append(category)
+                print("category",category)
                 driver.close()
                 driver.switch_to.window(parent)
                 time.sleep(5)
                 x=0
                 break
             x+=1
-        if cnt==5:
+        if cnt==no_of_products-1:
             break
         cnt+=1
         time.sleep(5)
@@ -134,28 +162,32 @@ def get_prd_flpkrt(search,feature):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(5)  # sleep_between_interactions
     driver.close()
-    # traverse list
     print(prd_details)
-    # if feature==1:
-    #     srt=sorted(prd_details["price"].values)
-    # else:
-    #     srt = sorted(prd_details["price"].values,reverse=True)
-    product_details=pd.DataFrame(prd_details)
+
+
+    return prd_details
+def main():
+
+    search=input("Enter what you want to search:")
+    feature=int(input("1:price high to low\n2:price low to high\n =>"))
+    if isinstance(feature, int):
+        pass
+    else:
+        print("please enter a right choise")
+    no_of_products=int(input("Enter the no. of products you want to scrap:"))
+    prd_details=get_prd_amazon(search, no_of_products)
+    prd_details1=get_prd_flpkrt(search, no_of_products)
+    for key in prd_details1:
+        prd_details[key].extend(prd_details1[key])
+    product_details = pd.DataFrame(prd_details)
+
     if feature == 1:
         product_details.sort_values(by='price', ascending=False)
     else:
         product_details.sort_values(by=['price'])
-    data_to_xl=pd.ExcelWriter('product_details.xlsx')
-    # file_name='product_details.xlsx'
+    data_to_xl = pd.ExcelWriter('product_details.xlsx')
     product_details.to_excel(data_to_xl)
     data_to_xl.save()
-    return
-search=input("Enter want you want to search:")
-feature=int(input("1:price high to low\n2:price low to high\n =>"))
-if isinstance(feature, int):
-    pass
-else:
-    print("please enter a right choise")
-# get_prd_flpkrt(search)
-get_prd_flpkrt(search,feature)
 
+if __name__=="__main__":
+    main()
